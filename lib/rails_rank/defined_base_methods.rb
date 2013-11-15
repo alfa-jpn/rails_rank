2bd5cf03e8a5cd2a8b63c2bf7e78553ed49b9a5e
alfa-jpn <a.nkmr.ja@gmail.com>
2013-11-15 19:24:53 +0900

    Refactoring the defined_base_methods.rb.

--- lib/rails_rank/defined_base_methods.rb
+++ lib/rails_rank/defined_base_methods.rb
@@ -3,17 +3,19 @@ require 'rails_rank/types/date'
 module RailsRank
   module DefinedBaseMethods
 
-    # get all time  slot(s).
+    # get all time slot(s).
     #
-    # @param date_type  [RailsRank::Types::Date]  delete type.(default=HOURLY)
+    # @param date_type  [RailsRank::Types::Date] delete type.(default=HOURLY)
+    # @param session    [RailsKvsDriver::Base]   default is nil. if there's session instance, set it.
     # @return [Array<Time>] all time slot(s).
-    def all(date_type=RailsRank::Types::Date::HOURLY)
-      rails_kvs_driver::session(rails_kvs_driver_config) do |kvs|
-        times      = Array.new
+    def all(date_type=RailsRank::Types::Date::HOURLY, session=nil)
+      start_kvs_session(session) do |kvs|
+        times   = Array.new
+        all_key = (date_type == RailsRank::Types::Date::ALL)
 
         kvs.sorted_sets.each do |key|
           key_a = key.split('-')
-          times.push(Time.local(*key_a)) if (date_type == RailsRank::Types::Date::ALL) or key_a.length == date_type
+          times.push(Time.local(*key_a)) if all_key or key_a.length == date_type
         end
 
         times
@@ -24,37 +26,41 @@ module RailsRank
     #
     # @param time       [Time]                    delete time slot.
     # @param date_type  [RailsRank::Types::Date]  delete type.(default=HOURLY)
-    def delete(time, date_type=RailsRank::Types::Date::HOURLY)
-      rails_kvs_driver::session(rails_kvs_driver_config) do |kvs|
+    # @param session    [RailsKvsDriver::Base]    default is nil. if there's session instance, set it.
+    def delete(time, date_type=RailsRank::Types::Date::HOURLY, session=nil)
+      start_kvs_session(session) do |kvs|
         kvs.delete(key_name(time, date_type))
       end
     end
 
     # delete all ranking data.
-    def delete_all
-      rails_kvs_driver::session(rails_kvs_driver_config) do |kvs|
+    # @param session [RailsKvsDriver::Base] default is nil. if there's session instance, set it.
+    def delete_all(session=nil)
+      start_kvs_session(session) do |kvs|
         kvs.delete_all
       end
     end
 
     # execute the block of code for each ranking.
     #
-    # @param key     [String]  key of time slot.
-    # @param reverse [Boolean] order desc. [default=asc]
-    # @param limit   [Integer] The maximum size of the request at once.
-    # @param &block [{|value, score, absolute_position| ...}] block of exec code.
-    def each(key, reverse=false, limit=1000, &block)
-      rails_kvs_driver::session(rails_kvs_driver_config) {|kvs| kvs.sorted_sets[key].each(reverse, limit, &block) }
+    # @param key     [String]               key of time slot.
+    # @param reverse [Boolean]              order desc. [default=asc]
+    # @param limit   [Integer]              The maximum size of the request at once.
+    # @param session [RailsKvsDriver::Base] default is nil. if there's session instance, set it.
+    # @param &block  [{|value, score, absolute_position| ...}] block of exec code.
+    def each(key, reverse=false, limit=1000, session=nil, &block)
+      start_kvs_session(session) {|kvs| kvs.sorted_sets[key].each(reverse, limit, &block) }
     end
 
     # increment this time slot score of a value. (Time slot is hourly.)
     # @note when doesn't exist the value, set 'score' to value of score.
     #
-    # @param value [String]   value to record the score.
-    # @param score [Integer]  score to increment
+    # @param value   [String]               value to record the score.
+    # @param score   [Integer]              score to increment.
+    # @param session [RailsKvsDriver::Base] default is nil. if there's session instance, set it.
     # @return [Integer] score after increment
-    def increment(value, score=1)
-      rails_kvs_driver::session(rails_kvs_driver_config) do |kvs|
+    def increment(value, score=1, session=nil)
+      start_kvs_session(session) do |kvs|
         kvs.sorted_sets[key_name(Time.now)].increment(value, score).to_i
       end
     end
@@ -80,10 +86,11 @@ module RailsRank
     # get this time slot score of a value.(Time slot is hourly.)
     # @note when doesn't exist the value, return 0.
     #
-    # @param value [String]   value to record the score.
+    # @param value   [String]               value to record the score.
+    # @param session [RailsKvsDriver::Base] default is nil. if there's session instance, set it.
     # @return [Integer] score
-    def score(value)
-      rails_kvs_driver::session(rails_kvs_driver_config) do |kvs|
+    def score(value, session=nil)
+      start_kvs_session(session) do |kvs|
         score = kvs.sorted_sets[key_name(Time.now)][value]
         (score.nil?) ? 0 : score
       end
@@ -100,27 +107,42 @@ module RailsRank
       tabled_data_count = 0
       base_time = Time.local(*(Time.now.to_a.reverse[4..(3+date_type)]))
 
-      all(date_type).each do |data_time|
-        next unless data_time < base_time
+      start_kvs_session do |kvs|
+        all(date_type).each do |data_time|
+          next unless data_time < base_time
 
-        rails_kvs_driver::session(rails_kvs_driver_config) do |kvs|
           key       = key_name(data_time, date_type)
           total_key = key_name(data_time, date_type - 1)
 
-          # table data of the time slot.
-          kvs.sorted_sets[key].each do |member, score, position|
+          each(key, false, 1000, kvs) do |member, score, position|
             if RailsRank::Types::Date::YEARLY < date_type
               kvs.sorted_sets[total_key].increment(member, score)
             end
             after_table(date_type, data_time, member, score.to_i, position)
           end
 
-          kvs.sorted_sets.delete(key)
+          delete(data_time, date_type, kvs)
           tabled_data_count += 1
         end
       end
 
       return tabled_data_count
+    end
+
+
+    private
+    # start key-value store session.
+    # if session of params isn't nil, call &block with this session.
+    #
+    # @params session [RailsKvsDriver::Base] session.
+    # @params yield   [block] exec block with session.
+    # @return [Object] Evaluation of the block.
+    def start_kvs_session(session=nil)
+      if session.nil?
+        rails_kvs_driver::session(rails_kvs_driver_config) {|new_session| yield new_session }
+      else
+        yield session
+      end
     end
 
   end
